f14460d6e8e747aadd3cee5e593a1777e89ee707
Jack Spence <jackspence123456@gmail.com>
2023-12-10 21:59:33 +0200

    Added Click event handler on the `butInstall` element.

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -9,8 +9,17 @@ window.addEventListener("beforeinstallprompt", (event) => {
 	installButton.removeAttribute("hidden");
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener("click", async () => {});
+// Click event handler on the `butInstall` element
+butInstall.addEventListener("click", async () => {
+	if (!installPrompt) {
+		return;
+	}
+
+	const result = await installPrompt.prompt();
+	console.log(`Install prompt was: ${result.outcome}!`);
+	installPrompt = null;
+	installButton.setAttribute("hidden", "");
+});
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener("appinstalled", (event) => {});
